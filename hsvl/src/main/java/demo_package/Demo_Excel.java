05e3595857ec704d64bd9d24c96081b2974745be
lakshmi <lakshmi.chandrashekara@atmecs.com>
2019-06-07 12:30:08 +0530

    first commit

---
+++ hsvl/src/main/java/demo_package/Demo_Excel.java
@@ -0,0 +1,23 @@
+package demo_package;
+
+import demo_program.$missing$;
+import demo_program.WriteExcelFileExample;
+
+public class Demo_Excel {
+
+	private static final String FILE_PATH ="";
+	private static final WriteExcelFileExample INSTANCE = new WriteExcelFileExample();
+	 
+    public static WriteExcelFileExample getInstance() {
+        return INSTANCE;
+    }
+ 
+    private WriteExcelFileExample() {
+    }
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
